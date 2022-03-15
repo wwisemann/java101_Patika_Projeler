234f26671809172f91c3cf2af25d6928d12182ef
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2022-03-15 16:37:42 +0300

    Add files via upload

---
+++ Uygulama 24 Yıldızlarla Üçgen Yapımı/src/Yildizlarla_Ucgen.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Yildizlarla_Ucgen {
+    public static void main(String[] args) {
+
+        // Değişkenler
+        int a;
+
+        // Veri Alma
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        a = inp.nextInt();
+
+        // İşlemler
+        for (int i = 0; i <= a; i++){
+            for (int j = 0; j <= a-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2*i+1); k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = 0; i <= a; i++) {
+
+            for (int j = 0; j <= (i+1) ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < (2*(a-i)); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
