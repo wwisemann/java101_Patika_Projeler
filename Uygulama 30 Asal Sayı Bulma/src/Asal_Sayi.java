6f38f05ed6914456368dd0b61f9e578317364e32
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2022-03-15 19:52:59 +0300

    Add files via upload

---
+++ Uygulama 30 Asal Sayı Bulma/src/Asal_Sayi.java
@@ -0,0 +1,17 @@
+public class Asal_Sayi {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++){
+            int control = 0;
+            for (int k = 2; k < i; k++){
+                if (i % k == 0){
+                    control = 1;
+                    break;
+                }
+            }
+            if (control == 0){
+                System.out.print("\t" + i);
+            }
+        }
+    }
+}
